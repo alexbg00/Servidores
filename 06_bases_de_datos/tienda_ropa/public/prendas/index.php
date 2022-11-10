3d5ee566b07d1812225741349b9b85c56f3c8c93
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2022-11-10 12:54:48 +0100

    arreglo

--- 06_bases_de_datos/tienda_ropa/public/prendas/index.php
+++ 06_bases_de_datos/tienda_ropa/public/prendas/index.php
@@ -1,163 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-
-    <title>Listado de prendas</title>
+    <title>Index</title>
 </head>
-
 <body>
-    <!-- acceder a base_de_datos.php -->
-
-    <?php require "../../util/base_de_datos.php" ?>
     <div class="container">
+        <?php require '../../util/base_de_datos.php' ?>
         <?php require '../header.php' ?>
+        <br>
         <h1>Listado de prendas</h1>
-        <!-- boton que redireccione a insertar_prenda.php -->
-        <a class="btn btn-primary" href="insertar_prenda.php" style="margin:30px">Insertar Prenda</a>
+
         <div class="row">
-            <div class="col-12">
-                <table class="table table-striped  table-hover">
-                    <thead>
-                        <tr class="table-dark">
+            <div class="col-9">
+                <br>
+                <a class="btn btn-primary" href="insertar_prenda.php">Nueva prenda</a>
+                <br><br>
+                <table class="table table-striped table-hover">
+                    <thead class="table-dark">
+                        <tr>
                             <th>Nombre</th>
+                            <th></th>
                             <th>Talla</th>
                             <th>Precio</th>
-                            <th>Categoria</th>
-                            <th>Imagen</th>
-                            <th></th>
+                            <th>Categoría</th>
                             <th></th>
                             <th></th>
                         </tr>
                     </thead>
-
                     <tbody>
-                        <?php
-                        $sql = "SELECT * FROM prendas";
-                        $resultado = $conexion->query($sql);
-                        ?>
-                        <!-- BORRAR PRENDA -->
+                        <?php   //  Borrar prenda
+                            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                                $id = $_POST["id"];
 
-                        <?php
-                        /* eliminar prenda */
+                                //  Consulta para coger la ruta de la imagen y luego borrarla
+                                $sql = "SELECT imagen FROM prendas WHERE id = '$id'";
+                                $resultado = $conexion -> query($sql);
 
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            $imagen = $_POST["imagen"];
-                            $id = $_POST["id"];
-                            $sql = "DELETE FROM prendas WHERE id = $id";
-                            $conexion->query($sql);
+                                if ($resultado -> num_rows > 0) {
+                                    while ($fila = $resultado -> fetch_assoc()) {
+                                        $imagen = $fila["imagen"];
+                                    }
+                                    unlink("../.." . $imagen);
+                                }
 
-                            unlink("../..$imagen");
-                        }
+                                //  Consulta para borrar la prenda
+                                $sql = "DELETE FROM prendas WHERE id = '$id'";
 
-                        ?>
-
-                        <?php
-                        $sql = "SELECT * FROM prendas";
-                        $resultado = $conexion->query($sql);
-                        if ($resultado->num_rows > 0) {
-                            while ($fila = $resultado->fetch_assoc()) { ?>
-                                <tr>
-                                    <td><?php echo $fila["nombre"] ?></td>
-                                    <td><?php echo $fila["talla"] ?></td>
-                                    <td><?php echo $fila["precio"] ?></td>
-                                    <td><?php echo $fila["categoria"] ?></td>
-                                    <td><img src="../../<?php echo $fila["imagen"] ?>" alt="" width="50px"></td>
-                                    <!-- Introducir boton  -->
-                                    <td>
-                                        <form action="mostrar_prenda.php" method="get">
-                                            <button class="btn btn-primary" type="submit">Ver</button>
-                                            <input type="hidden" name="id" value="<?php echo $fila["id"] ?>">
-                                        </form>
-                                    </td>
-                                    <td>
-                                        <form action="" method="post">
-                                            <button class="btn btn-danger" type="submit">🗑️</button>
-                                            <input type="hidden" name="id" value="<?php echo $fila["id"] ?>">
-                                            <input type="hidden" name="imagen" value="<?php echo $fila["imagen"] ?>">
-
-                                        </form>
-                                    </td>
-                                    <td>
-                                        <form action="" method="post">
-                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" type="submit">📝</button>
-                                            <input type="hidden" name="id" value="<?php echo $fila["id"] ?>">
-                                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Modificar usuario</h1>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            <form>
-                                                                <div class="mb-3">
-                                                                    <label for="exampleInputEmail1" class="form-label">Nombre</label>
-                                                                    <!-- pasar nombre por post -->
-                                                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nombre" value="<?php echo $fila["nombre"] ?>">
-
-                                                                </div>
-                                                                <div class="mb-3">
-                                                                    <!-- poner select para las tallas -->
-                                                                    <label for="exampleInputPassword1" class="form-label">Talla</label>
-                                                                    <select name="talla" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                                                                        <option selected><?php echo $fila["talla"]?></option>
-                                                                        <option value="XS">XS</option>
-                                                                        <option value="S">S</option>
-                                                                        <option value="M">M</option>
-                                                                        <option value="L">L</option>
-                                                                        <option value="XL">XL</option>
-                                                                    </select>
-                                                                </div>
-                                                                <div class="mb-3">
-                                                                    <label for="exampleInputPassword1" class="form-label">Precio</label>
-                                                                    <input type="text" class="form-control" id="exampleInputPassword1" value="<?php echo $fila["precio"] ?>">
-                                                                </div>
-                                                                <div class="mb-3">
-                                                                    <label class="form-check-label" for="exampleCheck1">Categoria</label>
-                                                                    <select name="categoria" class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                                                                        <option value="" > <?php echo $fila["categoria"]?></option>
-                                                                        <option value="Pantalones">Pantalones</option>
-                                                                        <option value="Camisetas">Camiseta</option>
-                                                                        <option value="Accesorios">Accesorios</option>
-                                                                    </select>
-                                                                </div>
-                                                                <button type="submit" class="btn btn-primary">Submit</button>
-                                                            </form>
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                                                            <button type="button" class="btn btn-primary">Guardar</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </td>
-                                </tr>
-                        <?php
+                                if ($conexion -> query($sql)) {
+                                ?>
+                                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                                        Se ha borrado la prenda
+                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                    </div>
+                                <?php
+                                } else {
+                                    echo "<p>Error al borrar</p>";
+                                }
                             }
-                        }
+                        ?>
 
+                        <?php   //  Seleccionar todas las prendas
+                            $sql = "SELECT * FROM prendas";
+                            $resultado = $conexion -> query($sql);
+
+                            if ($resultado -> num_rows > 0) {
+                                while ($fila = $resultado -> fetch_assoc()) {
+                                    $nombre = $fila["nombre"];
+                                    $talla = $fila["talla"];
+                                    $precio = $fila["precio"];
+                                    $categoria = $fila["categoria"];
+                                    $imagen = $fila["imagen"];
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $nombre ?></td>
+                                        <td>
+                                            <img width="50" height="60" src="../..<?php echo $imagen ?>">
+                                        </td>
+                                        <td><?php echo $talla ?></td>
+                                        <td><?php echo $precio ?></td>
+                                        <td><?php echo $categoria ?></td>
+                                        <td>
+                                            <form action="mostrar_prenda.php" method="get">
+                                                <button class="btn btn-primary" type="submit">Ver</button>
+                                                <input type="hidden" name="id" value="<?php echo $fila["id"] ?>">
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <form action="" method="post">
+                                                <button class="btn btn-danger" type="submit">Borrar</button>
+                                                <input type="hidden" name="id" value="<?php echo $fila["id"] ?>">
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
                         ?>
                     </tbody>
                 </table>
-
+            </div>
+            <div class="col-3">
+                <img width="200" heigth="200" src="../../resources/images/ropa.jpg">
             </div>
         </div>
-
     </div>
-
-
-
-
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
 </body>
-
 </html>
--- 06_bases_de_datos/tienda_ropa/public/prendas/mostrar_prenda.php
+++ 06_bases_de_datos/tienda_ropa/public/prendas/mostrar_prenda.php
@@ -8,45 +8,53 @@
     <title>Ver prenda</title>
 </head>
 <body>
-    <?php require "../../util/base_de_datos.php" ?>
-    <?php require "../header.php" ?>
     <div class="container">
-    <h1>Ver prenda</h1>
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        $id = $_GET["id"];
-        //echo "<p>$id</p>";
-    }
+        <?php require '../../util/base_de_datos.php' ?>
+        <?php require '../header.php' ?>
 
-    $sql = "SELECT * FROM prendas WHERE id = $id";
+        <h1>Ver prenda</h1>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "GET") {
+            $id = $_GET["id"];
 
-    $resultado = $conexion -> query($sql);
-    if($resultado -> num_rows>0){
-        while($fila = $resultado -> fetch_assoc()){
-        $nombre = $fila["nombre"];
-        $talla = $fila["talla"];
-        $precio = $fila["precio"];
-        $categoria = $fila["categoria"];
-        $imagen = $fila["imagen"];
+            $sql = "SELECT * FROM prendas WHERE id = '$id'";
+
+            $resultado = $conexion -> query($sql);
+
+            if ($resultado -> num_rows > 0) {
+                while ($fila = $resultado -> fetch_assoc()) {
+                    $nombre = $fila["nombre"];
+                    $talla = $fila["talla"];
+                    $precio = $fila["precio"];
+                    $categoria = $fila["categoria"];
+                    $imagen = $fila["imagen"];
+                }
+            }
         }
-    }
-    ?>
-    <div class="container">
-        <div class ="row">
-            <div class="col-3">
-                <h2 style="color:blue"><?php echo $nombre ?></h2>
-                <h4>Talla:</h4><p style="font-size:20px"><?php echo $talla ?></p>
-                <h4>Precio:</h4><p style="font-size:20px"><?php echo $precio ?>€</p>
-                <h4>Categoria:</h4><p style="font-size:20px"><?php echo $categoria ?></p>
-            </div>
-    
+        ?>
+
+        <div class="row">
             <div class="col-4">
-                <img width="auto" height="340" src="../../<?php echo $imagen ?>" alt="" >
+                <p>Nombre: <?php echo $nombre ?></p>
+                <p>Talla: <?php echo $talla ?></p>
+                <p>Precio: <?php echo $precio ?></p>
+                <p>Categoría: <?php echo $categoria ?></p>
+                <a class="btn btn-secondary" href="index.php">Volver</a>
+                <form action="editar_prenda.php" method="get">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <input type="hidden" name="nombre" value="<?php echo $nombre ?>">
+                    <input type="hidden" name="talla" value="<?php echo $talla ?>">
+                    <input type="hidden" name="precio" value="<?php echo $precio ?>">
+                    <input type="hidden" name="categoria" value="<?php echo $categoria ?>">
+                    <button type="submit" class="btn btn-primary">Editar</button>
+                </form>
+            </div>
+            <div class="col-4">
+                <img witdh="200" height="300" src="../..<?php echo $imagen ?>">
             </div>
         </div>
-        </div>
-        <a href="index.php" class="btn btn-primary">Volver</a>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 </html>
